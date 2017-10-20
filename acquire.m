2a86031a7c3ea8f3ae0a4ed302e99dfb2ff03979
mysablehats <mysablehats@gmail.com>
2017-10-20 12:27:26 +0100

    changed to logical indexing

--- acquire.m
+++ acquire.m
@@ -1,9 +1,24 @@
-function acquire()
- u1 = udp('10.188.20.187', 'RemotePort', 8866, 'LocalPort', 8844);
-fopen(u1);
-[vid,src] = startkinect();
-trigger(vid);
-[IM,~,metaData]=getdata(vid,1,'uint8');
-
-fprintf(u1, 'Ready for data transfer.')
+%function acquire()
+u2 = udp('10.188.18.215', 'RemotePort', 8844, 'LocalPort', 8866,'outputbuffersize',1024)
+if strcmp(u2.status,'closed')
+    fopen(u2);
 end
+[vid,src] = startkinect();
+start(vid);
+tracking = false;
+while(~tracking)
+    trigger(vid);
+    [IM,~,metaData]=getdata(vid,1,'uint8');
+    if any(metaData.IsSkeletonTracked)
+        tracking= true;
+    end
+end
+
+data = double(reshape(metaData.JointWorldCoordinates(:,:,metaData.IsSkeletonTracked),1,[]));
+fwrite(u2, data,'double')
+
+%% cleanup
+stop(vid)
+fclose(u2)
+
+%end
